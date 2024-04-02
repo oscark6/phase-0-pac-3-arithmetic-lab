a19868791ce863356a4170ea71944b454da7f582
Oscar <oscarkimani86@icloud.com>
2024-04-02 15:12:15 +0300

    arithmetic lab

---
+++ index.js
@@ -0,0 +1,71 @@
+function add() {
+}
+function subtract(){
+}
+function multiply() {
+}
+function divide(){
+}
+function add(a, b) {
+    return a + b;
+  }
+  
+  const sum = add(2, 4);
+  console.log(sum);
+
+function subtract(a, b) {
+    return a - b;
+  }
+  
+  console.log(subtract(4, 2)); 
+
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  console.log(multiply(3, 4)); 
+
+
+  function divide(a,b){
+    return a/b;
+  }
+
+
+
+  function increment(n) {
+    return n + 1;
+  }
+  
+  console.log(increment(5)); 
+
+
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  console.log(decrement(5)); 
+
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  console.log(makeInt('10')); 
+
+
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  console.log(makeInt('10'));
+
+
+  function preserveDecimall(n){
+    return parseFloat(n,10);
+  }
+
+  console.log(preserveDecimal(3.14));
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
